d376550fc3bb89b5823003d3328fa2b9d627460a
Tim Tate <timttate@gmail.com>
2015-03-30 00:02:54 -0700

    added LightController class

--- arduino_light_controller/arduino_light_controller.ino
+++ arduino_light_controller/arduino_light_controller.ino
@@ -1,13 +1,12 @@
 
 #include <IRremote.h>
+#include "LightController.h"
 
-const int RED_PIN = 9;
-const int GREEN_PIN = 3;
-const int BLUE_PIN = 10;
+#define RED_PIN 9
+#define GREEN_PIN 3
+#define BLUE_PIN 10
 
-int red_brightness = 0;
-int green_brightness = 0;
-int blue_brightness = 0;
+LightController light_controller(RED_PIN, GREEN_PIN, BLUE_PIN);
 
 #define IR_CODE_BRIGHTNESS_UP 0xFF906F
 #define IR_CODE_BRIGHTNESS_DOWN 0xFFB847
@@ -21,23 +20,9 @@ decode_results results;
 void setup()
 {
   Serial.begin(9600);
-  
-  pinMode(RED_PIN, OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
-  pinMode(BLUE_PIN, OUTPUT);
-  rgb_write(0, 0, 0);
+  light_controller.setup();
   
   irrecv.enableIRIn(); // Start the receiver
-}
-
-void rgb_write(int red, int green, int blue) {
-  red_brightness = red;
-  green_brightness = green;
-  blue_brightness = blue;
-  
-  analogWrite(RED_PIN, red);
-  analogWrite(GREEN_PIN, green);
-  analogWrite(BLUE_PIN, blue);
 }
 
 void loop() {
@@ -45,14 +30,20 @@ void loop() {
     if (results.bits != 0) {
       switch (results.value) {
         case IR_CODE_BRIGHTNESS_UP:
+          light_controller.bright_up();
+          Serial.println("up");
           break;
         case IR_CODE_BRIGHTNESS_DOWN:
+          light_controller.bright_down();
+          Serial.println("down");
           break;
         case IR_CODE_OFF:
-          rgb_write(0, 0, 0);
+          light_controller.turn_off();
+          Serial.println("off");
           break;
         case IR_CODE_ON:
-          rgb_write(16, 16, 16);
+          light_controller.turn_on();
+          Serial.println("on");
           break;
       }
     }
